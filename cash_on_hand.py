23898d5a8b71ffde9f461c2cdc51c794c50b1e3f
vap0r1x <S10242814@connect.np.edu.sg>
2022-08-03 23:10:57 +0800

    fixed comments

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,27 +1,28 @@
-def detect_cash_deficit(): # Function to Detect Cash Deficit
+# Function to Detect Cash Deficit
+def detect_cash_deficit():
 
-
-    with open("csv_reports/cash-on-hand-usd.csv","r") as file: # Open the file and read the lines
+# Open the file and read the lines
+    with open("csv_reports/cash-on-hand-usd.csv","r") as file: 
 
         lines = file.readlines()
-        
-        deficit_list = [] # set a variable for keeping track of cash deficit days
-        
-        for x in range(1,len(lines)): # loop all data 
-        
-            if x != 1: # Do nothing on first loop as there is nothing to compare
-                
-                curr_day = lines[x].split(",") # split current and previous day string into list 
+        # set a variable for keeping track of cash deficit days
+        deficit_list = [] 
+        # loop all data
+        for x in range(1,len(lines)): 
+        # Do nothing on first loop as there is nothing to compare
+            if x != 1: 
+                # split current and previous day string into list 
+                curr_day = lines[x].split(",") 
                 prev_day = lines[x-1].split(",")
-                
-                curr_day[1] = int(curr_day[1].rstrip()) # remove extra \n character from cash on hand amount and convert into int
+                # remove extra \n character from cash on hand amount and convert into int
+                curr_day[1] = int(curr_day[1].rstrip()) 
                 prev_day[1] = int(prev_day[1].rstrip())
-                
-                if curr_day[1] < prev_day[1]: # compare current and previous day cash on hand 
-                
-                    deficit_list.append([curr_day[0], prev_day[1] - curr_day[1]]) # if deficit detected, add day and amount to the deficit list to store
-                    
-        if len(deficit_list) == 0: # at the end of checking all lines, return deficit amounts or false if there are none
+                # compare current and previous day cash on hand 
+                if curr_day[1] < prev_day[1]: 
+                # if deficit detected, add day and amount to the deficit list to store
+                    deficit_list.append([curr_day[0], prev_day[1] - curr_day[1]]) 
+                    # at the end of checking all lines, return deficit amounts or false if there are none
+        if len(deficit_list) == 0: 
             return False
         else: 
             return deficit_list
--- overheads.py
+++ overheads.py
@@ -1,20 +1,22 @@
-def find_highest_overhead() : # function to find largest overhead
-    
-    with open("csv_reposrts/overheads-day-90.csv" , "r") as file: # open file and read lines
+# function to find largest overhead
+def find_highest_overhead() : 
+    # open file and read lines
+    with open("csv_reports/overheads-day-90.csv" , "r") as file: 
         lines = file.readlines()
-
-        overhead_name = [] # create list for name of overhead
-        overhead_amount = [] # create list for overhead amount
-
-        for x in range (1 ,len(lines)) : # loop through all the data and place them into the 2 lists above 
+# create list for name of overhead
+        overhead_name = [] 
+        # create list for overhead amount
+        overhead_amount = [] 
+# loop through all the data and place them into the 2 lists above 
+        for x in range (1 ,len(lines)) :
             # set x to the data of the current line and split it into a list
             x = lines[x] 
             x = x.split(",")
-
-            x[0] = x[0].replace('"',"") # place overhead name into its list and remove extra double apostrophes
+            # place overhead name into its list and remove extra double apostrophes
+            x[0] = x[0].replace('"',"") 
             overhead_name.append(x[0])
-
-            x[1] = float(x[1].rstrip()) # remove extra \n character from overhead amount and convert to int
+            # remove extra \n character from overhead amount and convert to int
+            x[1] = float(x[1].rstrip()) 
 
             overhead_amount.append(x[1]) # place overhead name into its list 
 
